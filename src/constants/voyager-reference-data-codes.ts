f170610f9b65bf79f3c5fd5e64895bc4397a520e
monsieurbelbo <gonzalo.bellver@gmail.com>
2023-09-24 17:29:01 +0200

    Voyager sex/species codes

---
+++ src/constants/voyager-reference-data-codes.ts
@@ -0,0 +1,10 @@
+export enum VoyagerSexes {
+  MALE = 'a6de9d9e-e3b9-4cc5-bfb4-68e33a8684ba',
+  FEMALE = 'bc93eac2-886a-47da-89b7-30e8e2d83e75',
+  UNKNOWN = 'b81354c6-9dca-46d1-91cb-b41c03ee3184'
+}
+
+export enum VoyagerSpecies {
+  CANINE = '36c3cde0-bd6b-11eb-9610-302432eba3e9',
+  FELINE = '29944158-bd6b-11eb-8276-302432eba3e9',
+}
