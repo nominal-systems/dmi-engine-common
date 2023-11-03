7db43c43e614bef4d5162aa349f2272910417195
Juan Ignacio Viglianco <jiviglianco@gmail.com>
2023-11-03 15:04:46 -0300

    Make providerError an error

--- src/interfaces/provider-error.interface.ts
+++ src/interfaces/provider-error.interface.ts
@@ -1,6 +1,15 @@
-export interface ProviderError {
+export interface ProviderErrorInterface {
   provider: string
   message: string
   code: number
   error: string
+}
+
+export class ProviderError extends Error {
+  response: ProviderErrorInterface
+  constructor (response) {
+    super(response.message)
+    this.name = 'ProviderError'
+    this.response = response
+  }
 }
